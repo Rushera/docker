d8d262fd6898acda30d5f6aff5d7a8e88bd9634d
rusher <rusherme@163.com>
2018-01-26 15:23:47 +0800

    update script

--- download.sh
+++ download.sh
@@ -2,6 +2,7 @@
 
 JENKINS_VERSION=1.609.2
 JENKINS_URL=https://repo.jenkins-ci.org/public/org/jenkins-ci/main/jenkins-war/${JENKINS_VERSION}/jenkins-war-${JENKINS_VERSION}.war
+JENKINS_SHA=59215da16f9f8a781d185dde683c05fcf11450ef
 
 curl -fsSL ${JENKINS_URL} -o jenkins-${JENKINS_VERSION}.war
-echo "${JENKINS_SHA}  jenkins-${JENKINS_VERSION}.war" | sha256sum -c -
+echo "${JENKINS_SHA}  jenkins-${JENKINS_VERSION}.war" | sha1sum -c -
